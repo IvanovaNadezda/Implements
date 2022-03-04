3425b652fdf0b2d2bd42e85f1b61759300e03ff2
Nadezhda <inadezda2003@gmail.com>
2022-03-04 21:18:52 +0300

    Initial commit

---
+++ src/com/company/AnimalTransport.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface AnimalTransport {
+    int legscount = 4;
+    void say();
+    void canSwim();
+    void canRun();
+    void canFly();
+}
---
+++ src/com/company/Cow.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Cow implements AnimalTransport{
+
+    @Override
+    public void say() {
+
+    }
+
+    @Override
+    public void canSwim() {
+
+    }
+
+    @Override
+    public void canRun() {
+
+    }
+
+    @Override
+    public void canFly() {
+
+    }
+}
